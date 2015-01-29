10ffd46c7365749c480a28325fcfa1c153df7911
Lopaka Delp <lopaka@rightscale.com>
2015-01-28 16:30:08 -0800

    initial creation with pseudo code.

---
+++ install_security_updates.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2015 RightScale, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+# Check if security updates are enabled.  If not, exit.
+if [[ $SECURITY_UPDATES != "enable" ]]; then
+  echo "Security Updates not enabled"
+  exit
+fi
+
+# on centos:
+# sed --in-place 's%/archive/20[0-9]*%/archive/latest%' /etc/yum.repos.d/*.repo
+# yum makecache
+# yum --security update
+
+# on ubuntu:
+# sed -i 's%ubuntu_daily/.* $(lsb_release -cs)-security%ubuntu_daily/latest $(lsb_release -cs)-security%' /etc/apt/sources.list.d/rightscale.sources.list
+# apt-get --yes update
+# apt-get --yes dist-upgrade
